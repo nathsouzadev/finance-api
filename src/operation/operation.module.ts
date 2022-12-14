028357d4cd8efce823f9b6348f50505d6f268f65
Nathally <nathallyccd@gmail.com>
2022-12-14 04:54:32 -0300

    [CHORE]-Update setup to run locally

--- src/operation/operation.module.ts
+++ src/operation/operation.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { OperationService } from './operation.service';
-import { PrismaService } from 'src/database/prisma.service';
+import { PrismaService } from '../database/prisma.service';
 import { OperationRepository } from './repository/operation.repository';
 import { PrismaOperationRepository } from './repository/prisma/prismaOperation.repository';
 
